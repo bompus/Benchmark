53d803053487680186df6c4df407d0419ac1f2d6
Aaron Q <bompus@users.noreply.github.com>
2017-06-30 10:24:36 -0700

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -69,6 +69,8 @@ function require_download() {
   fi
 }
 
+echo $FIO_DIR
+
 require_download FIO $FIO_DIR https://codeload.github.com/axboe/fio/tar.gz/fio-$FIO_VERSION
 require_download IOPing $IOPING_DIR https://codeload.github.com/koct9i/ioping/tar.gz/v$IOPING_VERSION
 require_download UnixBench $UNIX_BENCH_DIR https://github.com/bompus/Benchmark/raw/master/UnixBench$UNIX_BENCH_VERSION-patched.tgz
