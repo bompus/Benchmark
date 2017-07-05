919049f9d0772ae0110c765df0159ff889786401
Aaron Q <bompus@users.noreply.github.com>
2017-07-05 11:37:01 -0700

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -145,7 +145,7 @@ dd if=/dev/zero of=sb-io-test bs=64k count=16k conv=fdatasync
 echo "dd 1Mx1k dsync:"
 dd if=/dev/zero of=sb-io-test bs=1M count=1k oflag=dsync
 
-echo dd 64kx16k dsync:"
+echo "dd 64kx16k dsync:"
 dd if=/dev/zero of=sb-io-test bs=64k count=16k oflag=dsync
 
 rm -f sb-io-test
